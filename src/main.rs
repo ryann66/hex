4bd4150a09683c5ecc2a1afd1b2d33c7f39473ed
Ryan N <ryann66@cs.washington.edu>
2024-09-16 10:09:52 -0700

    Wrote high-level logic for read
    Converted fns read/write to accept args by value
    Added derive copy for structs

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::{process::exit, vec::Vec, io};
 use queues::{CircularBuffer, IsQueue};
 use bitvec::prelude::BitVec;
 
+#[derive(PartialEq, Eq)]
+#[derive(Clone, Copy)]
 enum ReadMode {
 	Binary,
 	Decimal,
@@ -11,6 +13,7 @@ enum ReadMode {
 	Interpret
 }
 
+#[derive(Clone, Copy)]
 enum WriteMode {
 	Binary,
 	Decimal,
@@ -18,6 +21,7 @@ enum WriteMode {
 	Octal
 }
 
+#[derive(Clone, Copy)]
 enum WriteLength {
 	Unfixed,
 	Fixed(u64)
@@ -57,14 +61,314 @@ fn print_help() {
    bitvec[0] is the 1s position and bitvec[2] would be the 4s position
  */
 
+ /**
+    adds the number represented by the addend to the number represented by sum
+  */
+fn add_bits_to(sum:&mut BitVec, addend:&BitVec) {
+	todo!()
+}
+
+/**
+   multiplies the number represented by bits by ten
+ */
+fn multiply_by_ten(bits:&mut BitVec) {
+	todo!()
+}
+
 /**
    Attempts to parse the string arg into an integer
    The result integer is returned as a little-endian integer (signedness indicated by signed_mode)
    On failure, returns an Err with an error message
  */
 #[inline]
-fn read(arg: &String, read_mode: &ReadMode, signed_mode: bool) -> Result<BitVec, String> {
-	Ok(BitVec::new())
+fn read(arg: &String, mut read_mode: ReadMode, signed_mode: bool) -> Result<BitVec, String> {
+	let mut negative_arg = false;
+	// strip all prefixes from the arg and interpret
+	let stripped_arg = {
+		// strip negative sign from the arg
+		let positive_arg = {
+			if let Some(tmp_arg) = arg.strip_prefix('-') {
+				negative_arg = true;
+				tmp_arg
+			} else {
+				arg
+			}
+		};
+
+		// strip prefix depending on read_mode
+		match read_mode {
+			ReadMode::Binary => {
+				if let Some(tmp_arg) = positive_arg.strip_prefix("0b") {
+					tmp_arg
+				} else {
+					positive_arg
+				}
+			}
+			ReadMode::Hex => {
+				if let Some(tmp_arg) = positive_arg.strip_prefix("0x") {
+					tmp_arg
+				} else {
+					positive_arg
+				}
+			}
+			ReadMode::Octal => {
+				if let Some(tmp_arg) = positive_arg.strip_prefix("0o") {
+					tmp_arg
+				} else {
+					positive_arg
+				}
+			}
+			ReadMode::Decimal => {
+				positive_arg
+			}
+			ReadMode::Interpret => {
+				if let Some(tmp_arg) = positive_arg.strip_prefix("0b") {
+					read_mode = ReadMode::Binary;
+					tmp_arg
+				} else if let Some(tmp_arg) = positive_arg.strip_prefix("0x") {
+					read_mode = ReadMode::Hex;
+					tmp_arg
+				} else if let Some(tmp_arg) = positive_arg.strip_prefix("0o") {
+					read_mode = ReadMode::Octal;
+					tmp_arg
+				} else if positive_arg.contains(['a', 'b', 'c', 'd', 'e', 'f', 'A', 'B', 'C', 'D', 'E', 'F']) {
+					read_mode = ReadMode::Hex;
+					positive_arg
+				} else {
+					read_mode = ReadMode::Decimal;
+					positive_arg
+				}
+			}
+		}
+	};
+	
+	// check negative arguments make sense
+	if !signed_mode && negative_arg {
+		return Err("Negative numbers not allowed in unsigned mode".to_string());
+	}
+	if read_mode != ReadMode::Decimal && negative_arg {
+		return Err("- operator is only allowed with decimal numbers".to_string());
+	}
+
+	let mut bits = BitVec::new();
+	match read_mode {
+		ReadMode::Binary => {
+			for c in stripped_arg.chars() {
+				match c {
+					'0' => bits.push(false),
+					'1' => bits.push(true),
+					c => return Err(format!("Character {} not allowed in binary numbers", c))
+				};
+			};
+		}
+		ReadMode::Octal => {
+			for c in stripped_arg.chars() {
+				match c {
+					'0' => {
+						bits.push(false);
+						bits.push(false);
+						bits.push(false);
+					}
+					'1' => {
+						bits.push(true);
+						bits.push(false);
+						bits.push(false);
+					}
+					'2' => {
+						bits.push(false);
+						bits.push(true);
+						bits.push(false);
+					}
+					'3' => {
+						bits.push(true);
+						bits.push(true);
+						bits.push(false);
+					}
+					'4' => {
+						bits.push(false);
+						bits.push(false);
+						bits.push(true);
+					}
+					'5' => {
+						bits.push(true);
+						bits.push(false);
+						bits.push(true);
+					}
+					'6' => {
+						bits.push(false);
+						bits.push(true);
+						bits.push(true);
+					}
+					'7' => {
+						bits.push(true);
+						bits.push(true);
+						bits.push(true);
+					}
+					c => return Err(format!("Character {} not allowed in octal numbers", c))
+				};
+			};
+		}
+		ReadMode::Hex => {
+			for c in stripped_arg.chars() {
+				match c {
+					'0' => {
+						bits.push(false);
+						bits.push(false);
+						bits.push(false);
+						bits.push(false);
+					}
+					'1' => {
+						bits.push(true);
+						bits.push(false);
+						bits.push(false);
+						bits.push(false);
+					}
+					'2' => {
+						bits.push(false);
+						bits.push(true);
+						bits.push(false);
+						bits.push(false);
+					}
+					'3' => {
+						bits.push(true);
+						bits.push(true);
+						bits.push(false);
+						bits.push(false);
+					}
+					'4' => {
+						bits.push(false);
+						bits.push(false);
+						bits.push(true);
+						bits.push(false);
+					}
+					'5' => {
+						bits.push(true);
+						bits.push(false);
+						bits.push(true);
+						bits.push(false);
+					}
+					'6' => {
+						bits.push(false);
+						bits.push(true);
+						bits.push(true);
+						bits.push(false);
+					}
+					'7' => {
+						bits.push(true);
+						bits.push(true);
+						bits.push(true);
+						bits.push(false);
+					}
+					'8' => {
+						bits.push(false);
+						bits.push(false);
+						bits.push(false);
+						bits.push(true);
+					}
+					'9' => {
+						bits.push(true);
+						bits.push(false);
+						bits.push(false);
+						bits.push(true);
+					}
+					'A' | 'a' => {
+						bits.push(false);
+						bits.push(true);
+						bits.push(false);
+						bits.push(true);
+					}
+					'B' | 'b' => {
+						bits.push(true);
+						bits.push(true);
+						bits.push(false);
+						bits.push(true);
+					}
+					'C' | 'c' => {
+						bits.push(false);
+						bits.push(false);
+						bits.push(true);
+						bits.push(true);
+					}
+					'D' | 'd' => {
+						bits.push(true);
+						bits.push(false);
+						bits.push(true);
+						bits.push(true);
+					}
+					'E' | 'e' => {
+						bits.push(false);
+						bits.push(true);
+						bits.push(true);
+						bits.push(true);
+					}
+					'F' | 'f' => {
+						bits.push(true);
+						bits.push(true);
+						bits.push(true);
+						bits.push(true);
+					}
+					c => return Err(format!("Character {} not allowed in hexadecimal numbers", c))
+				};
+			};
+		}
+		ReadMode::Decimal => {
+			for c in stripped_arg.chars() {
+				multiply_by_ten(&mut bits);
+				let mut addend = BitVec::new();
+				match c {
+					'0' => todo!(),
+					'1' => {
+						addend.push(true);
+					}
+					'2' => {
+						addend.push(false);
+						addend.push(true);
+					}
+					'3' => {
+						addend.push(true);
+						addend.push(true);
+					}
+					'4' => {
+						addend.push(false);
+						addend.push(false);
+						addend.push(true);
+					}
+					'5' => {
+						addend.push(true);
+						addend.push(false);
+						addend.push(true);
+					}
+					'6' => {
+						addend.push(false);
+						addend.push(true);
+						addend.push(true);
+					}
+					'7' => {
+						addend.push(true);
+						addend.push(true);
+						addend.push(true);
+					}
+					'8' => {
+						addend.push(false);
+						addend.push(false);
+						addend.push(false);
+						addend.push(true);
+					}
+					'9' => {
+						addend.push(true);
+						addend.push(false);
+						addend.push(false);
+						addend.push(true);
+					}
+					c => return Err(format!("Character {} not allowed in decimal numbers", c))
+				};
+				add_bits_to(&mut bits, &addend);
+			}
+		}
+		ReadMode::Interpret => assert!(false)
+	};
+	
+	return Ok(bits);
 }
 
 /**
@@ -72,7 +376,7 @@ fn read(arg: &String, read_mode: &ReadMode, signed_mode: bool) -> Result<BitVec,
    a string version of the integer in the format given by write_mode
  */
 #[inline]
-fn write(bits: &BitVec, write_mode: &WriteMode, write_length: &WriteLength, signed_mode: bool) -> String {
+fn write(bits: &BitVec, write_mode: WriteMode, write_length: WriteLength, signed_mode: bool) -> String {
 	String::new()
 }
 
@@ -80,7 +384,7 @@ fn write(bits: &BitVec, write_mode: &WriteMode, write_length: &WriteLength, sign
    Converts the given argument into the specified format and returns either the converted string or an error message
  */
 #[inline]
-fn convert(ref arg: String, read_mode: &ReadMode, write_mode: &WriteMode, write_length: &WriteLength, signed_mode: bool) -> Result<String, String> {
+fn convert(ref arg: &String, read_mode: ReadMode, write_mode: WriteMode, write_length: WriteLength, signed_mode: bool) -> Result<String, String> {
 	match read(arg, read_mode, signed_mode) {
 		Ok(bits) => {
 			Ok(write(&bits, write_mode, write_length, signed_mode))
@@ -166,7 +470,7 @@ fn main() {
 			}
 			_ => {
 				// something else (assume number)
-				match convert(arg, &read_mode, &write_mode, &write_length, signed_mode) {
+				match convert(&arg, read_mode, write_mode, write_length, signed_mode) {
 					Ok(str) => {
 						let _ = results.add(str);
 					}
@@ -198,7 +502,7 @@ fn main() {
 			Ok(_) => {
 				// presumed number
 				let _ = line.split_off(line.len() - 2);
-				match convert(line, &read_mode, &write_mode, &write_length, signed_mode) {
+				match convert(&line, read_mode, write_mode, write_length, signed_mode) {
 					Ok(str) => {
 						println!("{}", str);
 					}
